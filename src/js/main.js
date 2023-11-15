19325c2cc6225f7b43bf37468f9a7cc569c364a6
Matilda <Matilda.Wallin@medieinstitutet.se>
2023-11-15 14:07:49 +0100

    created doglist

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,36 @@
 import './../css/style.scss'
 
+class Dogs {
+  name;
+  breed;
+  age;
+
+  constructor (name, breed, age){
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+}
+
+const a = new Dogs ("Steffi", "German Shepherd mix", 8);
+const b = new Dogs ("Dixie", "German Shepherd", 12 );
+const c = new Dogs ("Tomtom", "Greyhound", 10);
+
+const app = document.getElementById ("app");
+const dogContainer = document.getElementById ("dogcontainer");
+const dogList = [a, b, c];
+
+app.appendChild (dogContainer);
+
+for (let i = 0; i < dogList.length; i++){
+  const divBox = document.createElement ("div");
+  const pTag = document.createElement ("p");
+  pTag.innerHTML = dogList[i].name +":" + " " + dogList[i].breed + "," + " " + dogList[i].age;
+
+  divBox.className = "dogbox";
+
+  dogContainer.appendChild (divBox);
+  divBox.appendChild (pTag);
+}
+
 
