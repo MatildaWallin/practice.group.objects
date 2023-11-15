b5e1faa5ddb513c6abe961abdf23fb16c9e0d5ec
WilliamWibeck <william.wibeck@gmail.com>
2023-11-15 14:08:08 +0100

    La till lista

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 import './../css/style.scss'
 
+let people = [
+    {name: "Noah", age: 24},
+    {name: "Viktor", age: 32},
+    {name: "Sara", age: 27}
+]
 
+for(let i = 0; i < people.length; i++){
+    let ptag = document.createElement("p");
+
+    ptag.innerHTML = people[i].name;
+
+    const ptagHolder = document.getElementById("ptagHolder");
+
+    ptagHolder.appendChild(ptag)
+}
